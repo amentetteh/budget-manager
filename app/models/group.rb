fda5e6efe73fc5db797efd8a03b21ce92dc9e8b1
Amen TETTEH <tkamen@outlook.com>
2023-06-03 13:20:50 +0000

    Generate group model

---
+++ app/models/group.rb
@@ -0,0 +1,9 @@
+class Group < ApplicationRecord
+    belongs_to :author, foreign_key: 'author_id', class_name: 'User'
+    has_and_belongs_to_many :expenses
+  
+    has_one_attached :icon
+  
+    validates :name, presence: true, length: { maximum: 120 }
+    validates :icon, presence: true, length: { maximum: 250 }
+  end
