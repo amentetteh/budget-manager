a1973d4ebdad6875c55d00df8f1e041082871ca4
Amen TETTEH <tkamen@outlook.com>
2023-06-03 13:25:13 +0000

    Generate expense  model

---
+++ app/models/expense.rb
@@ -0,0 +1,7 @@
+class Expense < ApplicationRecord
+    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+    has_and_belongs_to_many :groups
+  
+    validates :name, presence: true, length: { maximum: 120 }
+    validates :amount, numericality: { greater_than_or_equal_to: 0, only_float: true }
+  end
---
+++ db/migrate/20230603132222_create_expenses.rb
@@ -0,0 +1,10 @@
+class CreateExpenses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :expenses do |t|
+      t.references :author, foreign_key: { to_table: :users }, index: true
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
