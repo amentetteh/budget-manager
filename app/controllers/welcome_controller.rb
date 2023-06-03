527ecdfb3548806c261ac81515f806e1efa738f9
Amen TETTEH <tkamen@outlook.com>
2023-06-03 18:48:21 +0000

    Create the home page

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class WelcomeController < ApplicationController
+    skip_before_action :authenticate_user!
+    def index
+      redirect_to groups_path if user_signed_in?
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :welcome, only: [:index]
+  resources :groups do
+    resources :expenses
+  end
+  root "welcome#index"
 end
